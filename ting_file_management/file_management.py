be24609bc1652c7b7af83a3ff56a7519f67bce10
adevmir <ademirvalonqar@gmail.com>
2023-04-07 14:46:17 -0300

    [Req 02] - Implementa txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,10 @@
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    import sys
+    try:
+        if not path_file.endswith('.txt'):
+            print("Formato inválido", file=sys.stderr)
+            return
+        with open(path_file, 'r') as arquivo:
+            return arquivo.read().splitlines()
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
