2a787b2ea61204e3a37e0a52b2a9e313962947b0
adevmir <ademirvalonqar@gmail.com>
2023-04-10 17:05:53 -0300

    [Req 03] - Implementa funcao process em file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    content = txt_importer(path_file)
+    dict_content = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(content),
+        "linhas_do_arquivo": content
+    }
+    for e in range(0, len(instance)):
+        name_file = dict_content["nome_do_arquivo"]
+        if (instance.search(e)["nome_do_arquivo"] == name_file):
+            return
+    instance.enqueue(dict_content)
+    print(dict_content, file=sys.stdout)
 
 
 def remove(instance):
