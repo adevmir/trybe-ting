3c24f57b4b7a83a49995a7e63074d63fdbc5be4c
adevmir <ademirvalonqar@gmail.com>
2023-04-10 18:01:24 -0300

    [Req 07] - Implementa funcao exist_word em word_search.py

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    ocorrencias = list()
+    line_number = 0
+    for line in instance.search(0)["linhas_do_arquivo"]:
+        line_number += 1
+        if word.lower() in line.lower():
+            ocorrencias.append({"linha": line_number})
+    result = [{
+        "palavra": word,
+        "arquivo": instance.search(0)["nome_do_arquivo"],
+        "ocorrencias": ocorrencias
+    }]
+    if (ocorrencias == []):
+        return ocorrencias
+    return result
 
 
 def search_by_word(word, instance):
