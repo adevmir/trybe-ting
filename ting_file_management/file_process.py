245d9f3c3e9671220c4a6c6d990a1123d97e4416
adevmir <ademirvalonqar@gmail.com>
2023-04-10 17:13:07 -0300

    [Req 04] - Implementa a funcao remove em file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -18,7 +18,11 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        print("Não há elementos", file=sys.stdout)
+        return
+    path_file = instance.dequeue()["nome_do_arquivo"]
+    print(f"Arquivo {path_file} removido com sucesso", file=sys.stdout)
 
 
 def file_metadata(instance, position):
